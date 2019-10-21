77c5446d2fc5a6227eed5ee6c7c0af6dabeba5fd
Rodel Newton <rodel100@gmail.com>
2019-10-21 17:22:33 -0400

    Final Commit

--- javascriptfile.js
+++ javascriptfile.js
@@ -1,23 +1,51 @@
 let art = document.querySelector(".container")
-let i = 0
-let x = 257
-function makediv(x){
-for(i; i < x; i++){
-    let grid = document.createElement("div")    
-    grid.classList.add("grid")
-    grid.textContent = "";
-    art.appendChild(grid)
-}}
-makediv(x);
-let reset = document.querySelector('.reset');
-let grids = document.querySelectorAll('div');
-grids.forEach((div => {
+let x = 12;
+let buttonreset = document.querySelector('.reset');
+let buttonrainbow = document.querySelector(".rainbow");
+
+function makeDiv(x){
+    for(let i = 0; i < (x**2); i++){
+        let grid = document.createElement('div')
+        grid.classList.add("grid")
+        grid.innerHTML = "";
+        art.appendChild(grid);}
+    art.style.gridTemplateColumns = "repeat("+x+", 1fr)";}
+    function clear(){
+        let grids = document.querySelectorAll('div');
+        grids.forEach(div => {
+            div.classList.remove("grid")
+            div.remove();
+            })
+        let userinput = prompt('Enter a number');
+        intialize(userinput)}
+
+function colorenter(color){
+    let grids = document.querySelectorAll('div');
+    grids.forEach((div => {
     div.addEventListener("mouseenter", (e) => {
-    div.classList.add("colorchange");
-    });
-    reset.addEventListener("click", (e) => {
-        grids.forEach((div => {
-        div.classList.remove('colorchange');
-        }))
-        })
-}))
+    div.style.backgroundColor = color
+    })}))};
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+function randomcolor(){
+    let grids = document.querySelectorAll('div');
+    grids.forEach((div => {
+    div.addEventListener("mouseenter", (e) => {
+    div.style.backgroundColor = getRandomColor()
+    })}))};
+
+function colorblack() {
+ colorenter("black");}
+function intialize(x){
+makeDiv(x);
+colorenter("black");
+buttonrainbow.addEventListener("click", randomcolor);
+buttonreset.addEventListener("click", clear);
+}
+intialize(x);
